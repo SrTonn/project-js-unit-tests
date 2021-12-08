bc53925fffc3bc97e86f54a76622ce3b94e387d7
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-12-08 15:14:09 -0300

    requisito 02 concluido

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -15,11 +15,23 @@ const numbers = require('../src/numbers');
 */
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
-  it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
-    fail('Teste vazio!');
+  it('Verifica se a função `numbers` retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    expect(numbers([1, 2, 3, 4, 5])).toBe(true)
+  });
+
+  it('Verifica se a função `numbers` retorna `false` quando o array contém numero como string', () => {
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    expect(numbers([1, 2, '3', 4, 5])).toBe(false)
+  });
+
+  it('Verifica se a função `numbers` retorna `false` quando o array contém uma string', () => {
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    expect(numbers([1, 'a', 3])).toBe(false)
+  });
+
+  it('Verifica se a função `numbers` retorna `false` quando o array contém um espaço vazio', () => {
     // Escreva um teste em que a função recebe [' '] e retorna false
+    expect(numbers([' '])).toBe(false)
   });
 });
