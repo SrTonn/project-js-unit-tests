5f3fbd12762504dad9a09b8ff16bca9e34cf4bbd
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-12-08 22:19:17 -0300

    alteracoes do codereview

--- src/myCounter.js
+++ src/myCounter.js
@@ -18,10 +18,10 @@
 
 const myCounter = () => {
   const myArray = [];
-  for (let counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
-      myArray.push(counter2);
+  for (let index = 0; index <= 3; index += 1) {
+    myArray.push(index);
+    for (let counter = 2; counter <= 3; counter += 1) {
+      myArray.push(counter);
     }
   }
   return myArray;
