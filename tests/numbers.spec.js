4a7ed9ec623cbbe6fbbe804e228864316f2f72c5
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-12-13 20:00:39 -0300

    teste refatorado

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -17,12 +17,12 @@ const numbers = require('../src/numbers');
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers` retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    expect(numbers([1, 2, 3, 4, 5])).toBeTruthy()
+    expect(numbers([1, 2, 3, 4, 5])).toBe(true)
   });
 
   it('Verifica se a função `numbers` retorna `false` quando o array contém numero como string', () => {
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    expect(numbers([1, 2, '3', 4, 5])).toBeFalsy()
+    expect(numbers([1, 2, '3', 4, 5])).toBe(false)
   });
 
   it('Verifica se a função `numbers` retorna `false` quando o array contém uma string', () => {
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,23 +31,25 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
 
   it('Verifica se productDetails é uma função.', () => {
     // Teste se productDetails é uma função.
-    expect(typeof productDetails).toBe('function')
+    expect(productDetails).toBeInstanceOf(Function);
   });
 
   it('Verifica se o retorno da função é um array.', () => {
     // Teste se o retorno da função é um array.
-    expect(Array.isArray(result)).toBe(true)
+    expect(result).toBeInstanceOf(Object);
   });
 
   it('Verifica se o array retornado pela função contém dois itens dentro', () => {
     // Teste se o array retornado pela função contém dois itens dentro.
-    expect(result.length).toBe(2)
+    expect(result).toHaveLength(2)
   });
 
   it('Verifica se os dois itens dentro do array retornado pela função são objetos.', () => {
     // Teste se os dois itens dentro do array retornado pela função são objetos.
     expect(obj1).toBeInstanceOf(Object);
+    expect(obj1).not.toBeInstanceOf(Array);
     expect(obj2).toBeInstanceOf(Object);
+    expect(obj2).not.toBeInstanceOf(Array);
   });
 
   it('Verifica se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {
